6914da4b06ef82b028ba10a3bd53e44912fe01b7
clarisha <clarishhdwtyy@gmail.com>
2024-11-04 10:27:11 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('checkoutForm')?.addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert("Pemesanan sedang diproses. Terima kasih telah berbelanja di Glow in Me!");
+});
